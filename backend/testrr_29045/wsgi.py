040de0f6ba40b72b64fe7f946f5442275bb2e00e
Crowdbotics <team@crowdbotics.com>
2021-07-23 00:52:33 +0000

    Initial commit.

---
+++ backend/testrr_29045/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testrr_29045 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testrr_29045.settings')
+
+application = get_wsgi_application()
